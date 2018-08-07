9adf4a3c1a57e526c9b7af92ec4697e24e71eb54
kelleynurena@gmail.com Urena <kelleynurena@gmail.com>
2018-08-07 04:13:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,14 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray {
-var destructivelyAddElementToBeginningOfArray = 
+function addElementToBeginningOfArray(el, arr) {
+  const newArr = [...arr];
+  for(var i = 0; i < arr.length; i++) {
+    newArr[i] = arr[i];
+  }
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(el, arr) {
+  arr.unshift(el);
+  return arr;
+}
